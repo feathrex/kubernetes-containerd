b16dbf6505831a30fe745a469222e1d2126dda1f
Rex Featherston <rex.featherston@gmail.com>
2022-08-16 16:01:58 -0600

    Cleaned up files and directories.

--- dockerfiles/kube-decon/demo/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# cleanup prior to demo
-
-kubectl config use-context kubecon17
-kubectl delete -f resources/basic
-kubectl delete namespace webhelpers
-kubectl delete namespace kube-decon
--- dockerfiles/nginx-image/files/nginx.script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "<p>The host is: `uname -n`</p>" >> /usr/share/nginx/html/index.html
-echo "<p>The host has free space under / of: `df -h / `</p>" >> /usr/share/nginx/html/index.html
